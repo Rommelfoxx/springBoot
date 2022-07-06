e66c3a95216d8b8399d0bbe5f84bc9d224db9ab5
rommelfoxx <vanderlan.alves.filho@gmail.com>
2022-07-06 17:49:23 -0300

    Commit inicial

---
+++ src/main/java/io/github/Rommelfoxx/cliente/model/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package io.github.Rommelfoxx.cliente.model.repository;
+
+import io.github.Rommelfoxx.cliente.model.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
---
+++ src/main/java/io/github/Rommelfoxx/cliente/model/repository/ServicoRepository.java
@@ -0,0 +1,8 @@
+package io.github.Rommelfoxx.cliente.model.repository;
+
+import io.github.Rommelfoxx.cliente.model.entity.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+
+}
